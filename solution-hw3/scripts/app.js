80f136abd328f66ccb30c37f25e8c8a983c18b5b
ejchapma <ejchapma@andrew.cmu.edu>
2023-02-15 22:02:38 -0500

    hw 3

---
+++ solution-hw3/scripts/app.js
@@ -0,0 +1,49 @@
+//arrays
+let allGlazes = [
+    {
+        glaze: 'Keep original',
+        priceAdaptation: '0'
+    },
+    {
+        glaze: 'Sugar milk',
+        priceAdaptation: '0'
+    },
+    {
+        glaze: 'Vanilla milk',
+        priceAdaptation: '0.5',
+    },
+    {
+        glaze: 'Double milk',
+        priceAdaptation: '1.5',
+    }
+]
+
+//functions
+function glazeChange(element) {
+    //get value
+    const priceUpdate = element.value;
+    let glazePrice = parseFloat(allGlazes[priceUpdate].priceAdaptation);
+    let sizes = parseInt((document.querySelector('#sizes')).value);
+    let finalPrice = (2.49 + glazePrice)*sizes;
+    displayPrice("$" + finalPrice.toFixed(2));
+
+    console.log("finalPrice");
+    console.log(finalPrice);
+    console.log("glazePrice");
+    console.log(glazePrice);
+    console.log("size");
+    console.log(sizes);
+}
+//change display of final roll price 
+function displayPrice(rollPrice) {
+    let rollPriceDisplay = document.querySelector('#finalPrice');
+    rollPriceDisplay.innerText = rollPrice;
+}
+function sizeChange(element) {
+    //get value
+    const sizes = element.value;
+    let glazePriceIndex = parseInt(document.querySelector("#glazes").value);
+    let glazePrice = parseFloat(allGlazes[glazePriceIndex].priceAdaptation);
+    let totalPrice = (2.49 + glazePrice)*sizes;
+    displayPrice("$" + totalPrice.toFixed(2));
+}
